eb0565a94f57b365694465401d9a791944302445
Paul Nickerson <pvnick>
2023-04-15 22:50:24 -0400

    remove comment

--- terraform/oidc.tf
+++ terraform/oidc.tf
@@ -6,7 +6,3 @@ resource "aws_iam_openid_connect_provider" "github_actions" {
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
   provider        = aws.assumed
 }
-
-/*
-aws sts assume-role assume-role --role-arn arn:aws:iam::358258037662:role/iac --role-session-name assume-role
-*/
