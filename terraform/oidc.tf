6f35ba44bb5dfd13f6d474eec658fc31d3e677c6
Paul Nickerson <pvnick>
2023-04-17 08:33:38 -0400

    add terraform cloud to global identity providers

--- terraform/oidc.tf
+++ terraform/oidc.tf
@@ -5,3 +5,10 @@ resource "aws_iam_openid_connect_provider" "github_actions" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
+
+# add oidc provider to the new account for terraform cloud
+resource "aws_iam_openid_connect_provider" "terraform_cloud" {
+  url             = "https://app.terraform.io"
+  client_id_list  = ["aws.workload.identity"]
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
+}
